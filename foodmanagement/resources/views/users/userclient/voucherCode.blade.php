713ba21f0fe4860d95d95fb475b48669bf0ba24a
tanhung088 <tanhung.nguyen270799@gmail.com>
2022-12-09 18:42:24 +0700

    hungUpdatess_percent

--- foodmanagement/resources/views/users/userclient/voucherCode.blade.php
+++ foodmanagement/resources/views/users/userclient/voucherCode.blade.php
@@ -1,8 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
 
-<div class="alert alert-primary" role="alert">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>get_voucher</title>
+    <link rel="stylesheet" type="text/css"
+        href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css"
+        href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+    <style type="text/css">
+        body {
+            background: #f2f2f2;
+        }
+
+        .payment {
+            border: 1px solid #f2f2f2;
+            height: 280px;
+            border-radius: 20px;
+            background: #fff;
+        }
+
+        .payment_header {
+            background: rgba(255, 102, 0, 1);
+            padding: 20px;
+            border-radius: 20px 20px 0px 0px;
+
+        }
+
+        .check {
+            margin: 0px auto;
+            width: 50px;
+            height: 50px;
+            border-radius: 100%;
+            background: #fff;
+            text-align: center;
+        }
+
+        .check i {
+            vertical-align: middle;
+            line-height: 50px;
+            font-size: 30px;
+        }
+
+        .content {
+            text-align: center;
+        }
+
+        .content h1 {
+            font-size: 25px;
+            padding-top: 25px;
+        }
+
+        .content a {
+            width: 200px;
+            height: 35px;
+            color: #fff;
+            border-radius: 30px;
+            padding: 5px 10px;
+            background: rgba(255, 102, 0, 1);
+            transition: all ease-in-out 0.3s;
+        }
+
+        .content a:hover {
+            text-decoration: none;
+            background: #000;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 mx-auto mt-5">
+                <div class="payment">
+                    <div class="payment_header">
+                        <div class="check"><i class="fa fa-check" aria-hidden="true"></i></div>
+                    </div>
+                    <div class="content">
+                        <h1>Check Email to get Voucher !</h1>
+                        {{-- <p>Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print,
+                            graphic or web designs. </p> --}}
+                        <a href="http://gmail.com">Go to Email</a>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
+{{-- <div class="alert alert-primary" role="alert">
     <p>Check your email</p>
     <div>
-        <a href="{{route('user.product.all')}}">Shopping Now!</a>
+        <a href="https:\\gmail.com">Go to email</a>
     </div>
-</div>
-
+</div> --}}
